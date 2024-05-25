06c13c97ed18653812e43eec2cff0c1835c64886
huynhBaoMango <140953773+huynhBaoMango@users.noreply.github.com>
2024-05-25 15:56:13 +0700

    BAO code map

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator
+{
+    
+    List<RoomNode> allSpaceNodes = new List<RoomNode> ();
+    private int mapLength;
+    private int mapWidth;
+
+    public DungeonGenerator(int mapLength, int mapWidth)
+    {
+        this.mapLength = mapLength;
+        this.mapWidth = mapWidth;
+    }
+
+    public List<Node> CalculateRooms(int maxIterations, int roomWidthMin, int roomLengthMin)
+    {
+        BinarySpacePartitioner bsp = new BinarySpacePartitioner(mapWidth, mapLength);
+        allSpaceNodes = bsp.PrepareNodesCollection(maxIterations, roomWidthMin, roomLengthMin);
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapLength, mapWidth;
+    public int roomWidthMin, roomLengthMin;
+    public int maxIterations;
+    public int corridorWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDungeon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateDungeon()
+    {
+        DungeonGenerator generator = new DungeonGenerator(mapLength, mapWidth);
+        var listOfRooms = generator.CalculateRooms(maxIterations, roomWidthMin, roomLengthMin);
+    }
+}
