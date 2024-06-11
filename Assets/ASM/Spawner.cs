51f7a5ace5a0c922c945a3cbcc565e7e5d3bbcb5
huynhBaoMango <140953773+huynhBaoMango@users.noreply.github.com>
2024-06-11 16:58:12 +0700

    Update Spawner.cs

--- Assets/ASM/Spawner.cs
+++ Assets/ASM/Spawner.cs
@@ -24,13 +24,36 @@ public class Spawner : MonoBehaviour
 
     private bool bossSpawned;
     public Slider waveBar;
+<<<<<<< HEAD
     public TMP_Text waveText;
+=======
+    public Slider nextWaveBar; // Thêm thanh nextWaveBar
+    public TMP_Text waveText;
+    public TMP_Text nextWaveText;
+
+    public float countdownToWave = 60f; // Countdown timer for the current wave, adjustable from the Inspector
+    private float transitionTimer = 7f; // Timer for the next wave transition
+    private bool inTransition = false; // Flag to track if we are in transition
+>>>>>>> parent of 295e3b6 (dada)
 
     void Start()
     {
         player = GameObject.Find("PLAYER").transform;
+<<<<<<< HEAD
         countdownToWave = 60f;
         bossSpawned = false;
+=======
+        waveBar.maxValue = countdownToWave; // Set max value of waveBar
+        waveBar.value = countdownToWave; // Initialize waveBar to countdownToWave
+        nextWaveBar.maxValue = transitionTimer; // Set max value of nextWaveBar
+        nextWaveBar.value = transitionTimer; // Initialize nextWaveBar
+        UpdateWaveText();
+        nextWaveText.text = "";  // Initially hide next wave text
+
+        waveBar.gameObject.SetActive(true); // Bắt đầu với waveBar hiển thị
+        nextWaveBar.gameObject.SetActive(false); // Ẩn nextWaveBar ban đầu
+        nextWaveText.gameObject.SetActive(false); // Ẩn nextWaveText ban đầu
+>>>>>>> parent of 295e3b6 (dada)
     }
 
     // Update is called once per frame
@@ -49,17 +72,38 @@ public class Spawner : MonoBehaviour
         }
         else
         {
+<<<<<<< HEAD
+=======
+            // Transition to the next wave
+            wave++;
+            countdownToWave = 60f; // Reset countdownToWave to the set value
+            transitionTimer = 7f; // Reset the transition timer
+            waveBar.value = countdownToWave; // Reset waveBar
+            nextWaveBar.value = transitionTimer; // Reset nextWaveBar
+            nextWaveText.text = ""; // Hide next wave text
+            nextWaveText.gameObject.SetActive(false); // Hide nextWaveText after transition
+            waveText.gameObject.SetActive(true); // Show waveText after transition
+            waveBar.gameObject.SetActive(true); // Show waveBar after transition
+            nextWaveBar.gameObject.SetActive(false); // Hide nextWaveBar after transition
+            bossSpawned = false;
+            inTransition = false;
+>>>>>>> parent of 295e3b6 (dada)
             waveSpawn();
         }
 
         if (bossSpawned)
         {
+<<<<<<< HEAD
             if(bosses.transform.childCount == 0)
             {
                 countdownToWave = 60f;
                 wave++;
                 bossSpawned = false; 
             }
+=======
+            countdownToWave = 60f;
+            bossSpawned = false;
+>>>>>>> parent of 295e3b6 (dada)
         }
     }
 
@@ -94,7 +138,33 @@ public class Spawner : MonoBehaviour
             {
                 BossSpawn();
             }
+<<<<<<< HEAD
             bossSpawned = true; 
+=======
+
+            if (!bossSpawned)
+            {
+                int randomMaxBoss = Random.Range(1, wave + 1);  // Include the current wave in range
+                for (int i = 0; i < randomMaxBoss; i++)
+                {
+                    BossSpawn();
+                }
+                bossSpawned = true;
+            }
+        }
+    }
+
+    //het wave neu con enemy thi se giet het
+    void KillAllEnemies()
+    {
+        foreach (Transform enemy in enemies.transform)
+        {
+            Destroy(enemy.gameObject);
+        }
+        foreach (Transform boss in bosses.transform)
+        {
+            Destroy(boss.gameObject);
+>>>>>>> parent of 295e3b6 (dada)
         }
     }
 
@@ -179,4 +249,12 @@ public class Spawner : MonoBehaviour
         emanager.damage = (emanager.damage * 2) + (emanager.damage * wave * 0.1f);
         Instantiate(bossEffect, enemy.transform);
     }
+<<<<<<< HEAD
+=======
+
+    void UpdateWaveText()
+    {
+        waveText.text = "WAVE: " + wave;
+    }
+>>>>>>> parent of 295e3b6 (dada)
 }
