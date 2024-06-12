ba5684ace52302f25e0f0e802ab96b8e4d9cdd6e
hnghii <hnghi201003@gmail.com>
2024-06-12 13:31:07 +0700

    loading

--- Assets/SCI-FI UI Pack Pro/Common/Scripts/SliderRunTo1.cs
+++ Assets/SCI-FI UI Pack Pro/Common/Scripts/SliderRunTo1.cs
@@ -1,36 +1,58 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class SliderRunTo1 : MonoBehaviour
 {
- 
-     public bool b=true;
-	 public Slider slider;
-	 public float speed=0.5f;
+    public bool b = true;
+    public Slider slider;
+    public float speed = 0.5f;
+    public SliderValuePass sliderValuePass; // Tham chiếu đến script để cập nhật phần trăm
 
-  float time =0f;
-  
-  void Start()
-  {
-	  
-	slider = GetComponent<Slider>();
-  }
-  
+    private float time = 0f;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+
+        if (sliderValuePass == null)
+        {
+            // Tìm SliderValuePass trong cùng một GameObject hoặc tùy chỉnh để tìm đúng đối tượng
+            sliderValuePass = FindObjectOfType<SliderValuePass>();
+        }
+    }
+
     void Update()
     {
-		if(b)
-		{
-			time+=Time.deltaTime*speed;
-			slider.value = time;
-			
-        if(time>1)
-		{
-           
-			time=0;
-		}
+        if (b)
+        {
+            time += Time.deltaTime * speed;
+            slider.value = time;
+
+            // Cập nhật giá trị phần trăm
+            if (sliderValuePass != null)
+            {
+                sliderValuePass.UpdateProgress(time);
+            }
+
+            if (time >= 1)
+            {
+                StartCoroutine(LoadGameScene());
+            }
+        }
     }
-	}
-	
-	
+
+    // Coroutine để chuyển cảnh
+    IEnumerator LoadGameScene()
+    {
+        // Dừng thanh trượt
+        b = false;
+
+        // Đợi một chút để người dùng có thể thấy thanh trượt đầy 100%
+        yield return new WaitForSeconds(0.5f);
+
+        // Chuyển cảnh
+        SceneManager.LoadScene("NewMap"); // Đặt tên cảnh bạn muốn tải
+    }
 }
