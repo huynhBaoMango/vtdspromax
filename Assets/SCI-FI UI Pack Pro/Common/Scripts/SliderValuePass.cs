a39619cc06c2cef5d8afcab9b40b82f137dfc20e
Freakerq <150575354+Freakerq@users.noreply.github.com>
2024-06-12 13:37:08 +0700

    ghjk

---
+++ Assets/SCI-FI UI Pack Pro/Common/Scripts/SliderValuePass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValuePass : MonoBehaviour
+{
+    private Text progress;
+
+    void Start()
+    {
+        progress = GetComponent<Text>();
+    }
+
+    public void UpdateProgress(float content)
+    {
+        progress.text = Mathf.Round(content * 100) + "%";
+    }
+}
